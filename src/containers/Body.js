50be224b265c8f51749e0de3689ddac25ab7b1ff
Ilanit Fisher-Hinski <ilanit.fisher-hinski@sap.com>
2018-06-11 17:19:07 +0300

    update components

--- src/containers/Body.js
+++ src/containers/Body.js
@@ -1,17 +1,17 @@
 import React from "react";
 import '../css/App.css';
 import {jsonLocalCall} from "../apis/JsonLocalCall";
-import Tile from "./Tile";
+import Tile from "../components/Tile";
 
 class Body extends React.Component {
     render() {
         let mainJson = jsonLocalCall("main");
 
         let tilesElements = mainJson.categories.map((category) =>
-            <Tile tileId={category.id} tileName={category.name} tileUrl={"/word/" + category.id} imageName={category.imageName} />);
+            <Tile key={category.id} tileName={category.name} tileUrl={"/word/" + category.id} imageName={category.imageName} />);
         return (
             <div className="App-intro">
-                <div className="centerWidthAlign ">
+                <div className="centerWidthAlign">
                     <div className="fgTileContainer ">
                         {tilesElements}
                     </div>
