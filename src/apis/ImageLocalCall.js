554433bab5d1a17e93e23437c3c564d56e11dabe
Ilanit Fisher-Hinski <ilanit.fisher-hinski@sap.com>
2018-06-18 14:11:12 +0300

    Add Image loader

--- src/apis/ImageLocalCall.js
+++ src/apis/ImageLocalCall.js
@@ -4,44 +4,7 @@
  * @returns {*}
  */
 const getImageContent = (image):string => {
-    switch (image) {
-        case "adjective.png":
-            return require("../images/adjective.png");
-        case "animals.png":
-            return require("../images/animals.png");
-        case "body_parts.png":
-            return require("../images/body_parts.png");
-        case "food.png":
-            return require("../images/food.png");
-        case "nature.png":
-            return require("../images/nature.png");
-        case "occupations.png":
-            return require("../images/occupations.png");
-        case "time.png":
-            return require("../images/time.png");
-        case "vegetables.png":
-        return require("../images/vegetables.png");
-        case "verbs.png":
-            return require("../images/verbs.png");
-        case "greeting.png":
-            return require("../images/greeting.png");
-        case "people.png":
-            return require("../images/people.png");
-        case "can.png":
-            return require("../images/can.png");
-        case "love.png":
-            return require("../images/love.png");
-        case "אחות.png":
-            return require("../images/אחות.png");
-        case "בבקשה.png":
-            return require("../images/בבקשה.png");
-        case "בוקר טוב.png":
-            return require("../images/בוקר טוב.png");
-        case "חג שמח.png":
-            return require("../images/חג שמח.png");
-        default:
-            throw new Error("Image Not Found " + image);
-    }
+    return require("../images/" + image);
 };
 
 /**
