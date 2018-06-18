e15d70693523dfba1d7e2e248ac49d93ff903e9c
Ilanit Fisher-Hinski <ilanit.fisher-hinski@sap.com>
2018-06-18 19:40:58 +0300

    Add back button

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import {connect} from "react-redux";
 import { bindActionCreators } from "redux";
 
 import PropTypes from "prop-types";
+import {browserHistory} from "react-router";
 
 class App extends Component {
     render() {
+        let backElement = <div></div>;
+        if(this.props.location.pathname !== "/") {
+            backElement = <button slot="end-bar" className="zmdi zmdi-arrow-right"
+                              onClick={() => browserHistory.goBack()}></button>
+        }
         return (
             <div className="App">
                 <issie-shell theme="blue" id="page1" className="page">
-                             <button slot="start-bar" className="zmdi zmdi-info-outline"></button>
-                             <button slot="start-bar" className="zmdi zmdi-menu"></button>
-                             <h1 slot="title">שפת הסימנים</h1>
-                             <issie-search theme="blue" slot="end-bar"></issie-search>
+                    <button slot="start-bar" className="zmdi zmdi-info-outline"></button>
+                    <button slot="start-bar" className="zmdi zmdi-menu"></button>
+                    <h1 slot="title">שפת הסימנים</h1>
+                    <issie-search theme="blue" slot="end-bar"></issie-search>
+                    { backElement }
                 </issie-shell>
                 {this.props.children}
             </div>
