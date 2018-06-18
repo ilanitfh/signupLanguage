9767d40f6bb9660df489b7d3174ac8e77330ac82
Ilanit Fisher-Hinski <ilanit.fisher-hinski@sap.com>
2018-06-18 15:27:38 +0300

    Add all images + videos

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router";
 import "../css/Tile.css";
 import "../css/Common.css";
-import '../css/style.css';
 import {imageLocalCall} from "../apis/ImageLocalCall";
 
 const Tile = ({tileId, tileName, tileUrl, imageName}) => {
     let imageSrc = imageName ? imageLocalCall(imageName) : "image1.png";
     return (
         <Link to={tileUrl}>
-                 <issie-box theme="blue" theme-flavor="13">
+            <div className="tileBox">
+                <issie-box theme="blue" theme-flavor="13">
                     <h2 slot="title" className="rtl tileFont">{tileName}</h2>
-                    <img slot="body" width="100%" height="100%" src={imageSrc} alt="Category Placeholder"></img>
+                    <img slot="body" src={imageSrc} alt="Category Placeholder" width="100%" height="100%"></img>
                     <img slot="decor-top" src="assets/r-cube.svg" alt="cube"></img>
-                 </issie-box>
+                </issie-box>
+            </div>
         </Link>
     );
 };
