7a1acc6ffe320e210c870b36b6018135a5848acc
Ariel Bentolila <ariel.bentolila@sap.com>
2018-06-18 17:20:41 +0300

    fix cordova

--- scripts/createJson.js
+++ scripts/createJson.js
@@ -1,7 +1,7 @@
 console.log("Start building JSON file for sign language app");
 
 var sourceFile = '/Users/i022021/dev/sign_lang/content'
-var targetFile = '/tmp/sl.json'
+var targetFile = './sl.json'
 
 
 var fs = require('fs');
@@ -14,27 +14,42 @@ var fileIndex = 0;
 var items = fs.readdirSync(sourceFile);
 
 for (var i=0; items && i<items.length; i++) {
-    console.log(items[i]);
+//    console.log(items[i]);
 
     let category = {};
     category.name = items[i];
     category.id = categoryIndex.toString();
     category.imageName = items[i] + ".png"
     category.words = []
-    console.log("read words:" + sourceFile + '/' + category.name);
+//    console.log("read words:" + sourceFile + '/' + category.name);
     try {
         var words = fs.readdirSync(sourceFile + '/' + category.name);
         if (words) {
             fileIndex = categoryIndex * 100 + 1;
             for (var j=0; j<words.length; j++) {
+                var suffix = ""
                 if (words[j].endsWith(".mov")) {
-                    console.log(category.id + " " + words[j]);
+                    suffix = ".mov"
+                } else if (words[j].endsWith(".mp4")) {
+                    suffix = ".mp4"
+                }
+
+
+
+                if (suffix != "") {
+//                    console.log(category.id + " " + words[j]);
                     var word = {};
-                    word.name = words[j];
+                    word.name = words[j].replace(suffix, "");
                     word.id = fileIndex;
                     fileIndex++;
-                    word.imageName = word.name.replace(".mov", ".png");
-                    word.videoName = word.name.replace(".mov", "_x264.mov");
+                    word.imageName = words[j].replace(suffix, ".png");
+                    //test image exists
+                    var imagePath = sourceFile + '/' + category.name + "/סמלים/" + word.imageName
+                    if (!fs.existsSync(imagePath)) {
+                        console.log("missing: " , category.name, " - ", word.imageName);
+                        word.imageName = "no_image.png"
+                    }
+                    word.videoName = words[j].replace(suffix, "_x264.mov");
                     category.words.push(word);
                 }
             }
---
+++ scripts/generateImages.sh
@@ -0,0 +1,4 @@
+npm i cordova-icon-generator
+-s app_icons -t ./build/app_icons -t ios
+
+#https://makeappicon.com/download/bfe0ec333e1d431fbcbfa21abfcf6d15
