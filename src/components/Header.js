85944f43db2c78623985962cc6be0ca07396cd04
Ido Mosseri <ido.mosseri@sap.com>
2018-06-18 15:33:41 +0300

    sefat

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
           <issie-shell theme="blue" id="page1" className="page">
               <button slot="start-bar" className="zmdi zmdi-info-outline"></button>
               <button slot="start-bar" className="zmdi zmdi-menu"></button>
-              <h1 slot="title">ספת הסימנים - קטגוריות</h1>
+              <h1 slot="title">שפת הסימנים - קטגוריות</h1>
               <issie-search theme="blue" slot="end-bar"></issie-search>
 
           </issie-shell>
