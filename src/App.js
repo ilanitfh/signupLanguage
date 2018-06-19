1b93681716d6e3087312894d5a8a795df5b4f515
Borenshtein <liat.borenshtein@sap.com>
2018-06-19 11:49:21 +0300

    Change title and header color according to category id

--- src/App.js
+++ src/App.js
@@ -4,24 +4,63 @@ import './css/App.css';
 import './css/style.css';
 import {connect} from "react-redux";
 import { bindActionCreators } from "redux";
+import {jsonLocalCall} from "./apis/JsonLocalCall";
 
 import PropTypes from "prop-types";
 import {browserHistory} from "react-router";
 
 class App extends Component {
     render() {
+
+        let themeMap = {
+            "1": "flavor-0",
+            "2": "flavor-1",
+            "3": "flavor-2",
+            "4": "flavor-3",
+            "5": "flavor-4",
+            "6": "flavor-5",
+            "7": "flavor-6",
+            "8": "flavor-7",
+            "9": "flavor-8",
+            "10": "flavor-9",
+            "11": "flavor-10",
+            "12": "flavor-11",
+            "13": "flavor-12",
+            "14": "flavor-13",
+            "15": "flavor-14",
+            "16": "flavor-15",
+            "17": "flavor-16",
+            "18": "flavor-17",
+            "19": "flavor-18",
+            "20": "flavor-19",
+            "21": "flavor-20",
+            "22": "flavor-21",
+            "23": "flavor-22",
+            "24": "flavor-23",
+        };
+
         let backElement = <div></div>;
-        if(this.props.location.pathname !== "/") {
+        let categoryTheme = "blue"
+        let title = "שפת הסימנים"
+        let mainJson = jsonLocalCall("main");
+        let path = this.props.location.pathname;
+        if(path !== "/") {
             backElement = <button slot="end-bar" className="zmdi zmdi-arrow-right"
                               onClick={() => browserHistory.goBack()}></button>
         }
+        
+        if(path.startsWith("/word")){
+            let categoryId = this.props.params.wordId;
+            categoryTheme=themeMap[categoryId];
+            title = mainJson.categories[categoryId-1].name;
+        }
         return (
             <div className="App">
-                <issie-shell theme="blue" id="page1" className="page">
+                <issie-shell theme={categoryTheme} id="page1" className="page">
                     <button slot="start-bar" className="zmdi zmdi-info-outline"></button>
                     <button slot="start-bar" className="zmdi zmdi-menu"></button>
-                    <h1 slot="title">שפת הסימנים</h1>
-                    <issie-search theme="blue" slot="end-bar"></issie-search>
+                    <h1 slot="title">{title}</h1>
+                    <issie-search theme={categoryTheme} slot="end-bar"></issie-search>
                     <a slot="next" href="#page5"><img src="assets/arrow-right.svg" alt="arrow"/></a>
                     <a slot="prev" href="#page5"><img src="assets/arrow-left.svg" alt="arrow"/></a>
                     { backElement }
